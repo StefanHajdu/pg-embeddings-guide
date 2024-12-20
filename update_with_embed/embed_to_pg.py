9b4a578a4019d679bab3894f6aa7885dfd8851b6
StefanHajdu <stevo.hajdu006@gmail.com>
2024-12-20 14:37:29 +0100

    embed parallel

--- update_with_embed/embed_to_pg.py
+++ update_with_embed/embed_to_pg.py
@@ -1,28 +1,59 @@
 import asyncio as aio
 from ollama import AsyncClient
+import asyncpg as apg
+import psycopg2 as pg2
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 from typing import TypedDict
+import time
+from functools import wraps
 
 
 class DbItem(TypedDict):
     idx: str
-    text: int
+    text: str
+
+
+class DbUpdatedItem(TypedDict):
+    idx: str
+    embed: list[float]
 
 
 OLLAMA_PORT = 11_434
+
+
+def async_measure(coro):
+    @wraps(coro)
+    async def measure_wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = await coro(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{coro.__name__}: {(end - start)} s")
+        return result
+
+    return measure_wrapper
+
+
+def measure(func):
+    @wraps(func)
+    def measure_wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__}: {(end - start)} s")
+        return result
+
+    return measure_wrapper
 
 
 class OllamaSimplePool:
     def __init__(self):
         self.client = AsyncClient(host=f"http://localhost:{OLLAMA_PORT}")
 
-    def embed_bulk(self, bulk: list[DbItem]):
-        return aio.run(self.send_bulk(bulk))
-
-    async def send_bulk(self, bulk: list[DbItem]):
+    @async_measure
+    async def embed_bulk(self, bulk: list[DbItem]):
         responses = await aio.gather(
             *[self._embed_coro(self.client, item.get("text", "")) for item in bulk]
         )
@@ -35,9 +66,7 @@ class OllamaSimplePool:
         )
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
+def open_sqlalchemy_conn():
     host = ("localhost",)
     port = (os.environ["PG_PORT"],)
     database = ("pgvector-test",)
@@ -46,17 +75,92 @@ if __name__ == "__main__":
     connection_str = (
         f"postgresql://{user[0]}:{password[0]}@{host[0]}:{port[0]}/{database[0]}"
     )
+    return create_engine(connection_str)
 
-    ollamaPool = OllamaSimplePool()
-    engine = create_engine(connection_str)
-    with engine.connect() as conn:
+
+@async_measure
+async def update_pg_with_embed(
+    apg_conn_pool, bulk: dict[str:str], responses: list[float]
+):
+    updated_data = [
+        (str(response.embedding), str(bulk.get("idx")))
+        for bulk, response in zip(bulk, responses)
+    ]
+
+    apg_conn = await apg_conn_pool.acquire()
+
+    try:
+        await apg_conn.executemany(
+            """
+                UPDATE youtube_comments SET embed = $1 WHERE idx = $2
+            """,
+            updated_data,
+        )
+    except Exception as e:
+        print(e)
+    finally:
+        await apg_conn_pool.release(apg_conn)
+
+
+@measure
+def seq_update_pg_with_embed(pg_conn, bulk: dict[str:str], responses: list[float]):
+    updated_data = [
+        (str(response.embedding), str(bulk.get("idx")))
+        for bulk, response in zip(bulk, responses)
+    ]
+    cur = pg_conn.cursor()
+    cur.executemany(
+        """
+            UPDATE youtube_comments SET embed = %s WHERE idx = %s
+        """,
+        updated_data,
+    )
+    pg_conn.commit()
+
+
+@async_measure
+async def iterate_and_update(iterator_conn, ollamaPool, limit=2):
+    # apg_conn_pool = await apg.create_pool(
+    #     min_size=10,
+    #     max_size=10,
+    #     max_inactive_connection_lifetime=0,
+    #     host="localhost",
+    #     port=os.environ["PG_PORT"],
+    #     database="pgvector-test",
+    #     user=os.environ["PG_USER"],
+    #     password=os.environ["PG_PASSWORD"],
+    # )
+
+    pg_conn = pg2.connect(
+        host="localhost",
+        port=os.environ["PG_PORT"],
+        database="pgvector-test",
+        user=os.environ["PG_USER"],
+        password=os.environ["PG_PASSWORD"],
+    )
+
+    cnt = 0
+    with iterator_conn.connect() as conn:
         for df in pd.read_sql(
-            "SELECT idx, text FROM youtube_comments", conn, chunksize=10
+            "SELECT idx, text FROM youtube_comments", conn, chunksize=100
         ):
-            bulk = df.to_dict(orient="records")
-            responses = ollamaPool.embed_bulk(bulk=bulk)
+            s1 = time.perf_counter()
+            if cnt == limit:
+                break
+            else:
+                cnt += 1
+                bulk = df.to_dict(orient="records")
+                responses = await ollamaPool.embed_bulk(bulk=bulk)
+                # await update_pg_with_embed(apg_conn_pool, bulk, responses)
+                seq_update_pg_with_embed(pg_conn, bulk, responses)
+                print(f"iteration: {cnt} in {time.perf_counter() - s1}")
 
-            for b, r in zip(bulk, responses):
-                print(b["idx"], b["text"][:10], r.embedding[:5])
-                print()
-            break
+    # await apg_conn_pool.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    iterator_conn = open_sqlalchemy_conn()
+    ollamaPool = OllamaSimplePool()
+    aio.run(iterate_and_update(iterator_conn, ollamaPool))
--- update_with_embed/queries.sql
+++ update_with_embed/queries.sql
@@ -1,4 +1,19 @@
-select * from youtube_comments limit 10;
+select embed from youtube_comments limit 100;
+select embed from youtube_comments where idx = 'cc0a85e4-64dc-45ee-b212-9f4be765b5fe';
+select * from youtube_comments where idx = 'b0354709-4fb0-4c4d-9d68-05364eeb3ef8';
+
+
+ALTER TABLE youtube_comments DROP COLUMN embed; 
+ALTER TABLE youtube_comments ADD embed vector(768);
+
+DROP INDEX pk_index;
+CREATE UNIQUE INDEX pk_index
+    ON public.youtube_comments USING btree
+    (idx)
+    WITH (deduplicate_items=False)
+;
+
+select count(*) from youtube_comments where embed is not null;
 
 create extension if not exists "uuid-ossp";
 alter table youtube_comments add column idx uuid default (uuid_generate_v4());
